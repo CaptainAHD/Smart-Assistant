c57bebe69e8c38265e5e553f517fa439e83d1b4e
Aashay Dwivedi <aashay.dwivedi007@gmail.com>
2024-04-21 20:09:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ def stt(file_path):
   except Exception as e:
     return f"Exception: {e}"
 
-source_language = st.selectbox("Select Audio Language:", ["English", "Other"])
 audio_file = st.file_uploader("Upload an audio file", type=["mp3"])
 def save_uploaded_file(uploaded_file):
   with open("temp_audio.mp3", "wb") as f:
